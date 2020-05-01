9063b4c817f35dfe9fb63be2276079a5a2014ae1
Martin <martin.legrand@epitech.eu>
2020-05-01 12:26:27 +0200

    [fix] now cant fire when reloading

--- src/main_scene/player_attack.c
+++ src/main_scene/player_attack.c
@@ -29,9 +29,9 @@ static int switch_attack(game_t *game, int *r, sfRenderWindow *window)
 {
     char *animation = NULL;
 
-    if (*r == 2)
+    if (*r == ATTACK)
         return(on_attack(game, window, animation, r));
-    if (*r == 3) {
+    if (*r == RELOAD) {
         animation = append(game->player.selected, ":reload");
         if (compare(game->player.selected, "rifle"))
             game->player.ammo_rifle = 30;
@@ -39,8 +39,10 @@ static int switch_attack(game_t *game, int *r, sfRenderWindow *window)
             game->player.ammo_gun = 11;
         *r = starset_play_animation(game->player.save, "player", animation, 1);
         game->player.noise = 1.3;
-        if (*r == 1)
-            *r = 3;
+        if (*r == 1 || limit_rate_reload(2100) == 1)
+            *r = RELOAD;
+        else
+            *r = 0;
         return (7);
     }
     return (1);
@@ -53,8 +55,8 @@ int attack_entities(game_t *game, sfEvent *event
     int switch_r = 0;
 
     if (event->mouseButton.button == sfMouseLeft && !compare
-    (game->player.selected, "torch")) {
-        r = 2;
+    (game->player.selected, "torch") && r != RELOAD) {
+        r = ATTACK;
         event->mouseButton.button = -1;
     }
     if ((event->mouseButton.button == sfMouseRight ||
@@ -62,7 +64,7 @@ int attack_entities(game_t *game, sfEvent *event
     (game->player.selected, "torch") || compare(game->player.selected
     , "knife"))) {
         starset_single_play_sound(game->player.save, "reload", false);
-        r = 3;
+        r = RELOAD;
         event->mouseButton.button = -1;
     }
     switch_r = switch_attack(game, &r, window);
